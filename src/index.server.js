f0da0865a2293f263f52556781cc72157ca02f75
Akshit <akshitthecoder@gmail.com>
2022-10-02 18:33:59 +0530

    Backend Completed

---
+++ src/index.server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const env = require("dotenv");
+const mongoose = require("mongoose");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+
+// routes
+const authRoutes = require("./routes/auth");
+const adminRoutes = require("./routes/admin/auth");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const cartRoutes = require("./routes/cart");
+const initialDataRoutes = require("./routes/admin/initialData");
+const pageRoutes = require("./routes/admin/page");
+const addressRoutes = require("./routes/address");
+const orderRoutes = require("./routes/order");
+const adminOrderRoute = require("./routes/admin/order.routes");
+
+// environment variables
+env.config();
+
+//mongodb connection
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log("Database Connected Successfully ✅");
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use("/public", express.static(path.join(__dirname, "uploads")));
+app.use("/api", authRoutes);
+app.use("/api", adminRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", cartRoutes);
+app.use("/api", initialDataRoutes);
+app.use("/api", pageRoutes);
+app.use("/api", addressRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", adminOrderRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}  ✅`);
+});
